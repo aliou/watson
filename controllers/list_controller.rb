6fa27d7c8518374e1b7dbcba3e18c1619e54c512
Aliou Diallo <code@aliou.me>
2013-10-02 11:45:19 +0200

    Add route for updating lists.

--- controllers/list_controller.rb
+++ controllers/list_controller.rb
@@ -15,9 +15,24 @@ class Watson < Sinatra::Base
     erb :'lists/index'
   end
 
-  delete "/lists/:feed_id" do
+  put "/lists/:id" do
     begin
-      list = List.find(params[:feed_id])
+      list = List.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "List not found"
+      redirect to('/lists')
+    end
+
+    list.name = params[:name] if params.keys.include?(:name)
+    list.archive! if params.keys.include?(:archived_at)
+    list.save
+
+    erb :'lists/index'
+  end
+
+  delete "/lists/:id" do
+    begin
+      list = List.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       flash.now[:error] = "You are trying to delete an unexistant list. WTF?!"
       erb :'lists/index'
